480d5bf5813757c0adc45d3d42d9e16da1890b41
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-06-22 01:46:49 +0530

    functionality complete

--- assets/js/classifier_2.js
+++ assets/js/classifier_2.js
@@ -2,10 +2,8 @@
 * @Author: prabhakar
 * @Date:   2016-06-18 15:24:57
 * @Last Modified by:   Prabhakar Gupta
-* @Last Modified time: 2016-06-22 01:45:47
+* @Last Modified time: 2016-06-22 01:09:04
 */
-
-var user_repo_languages = [];
 
 function show_languages(language, mode, github_filter){
 	if(mode){
@@ -65,10 +63,10 @@ function add_language_count(obj, language){
 }
 
 function update_repo_info(){
-	user_repo_languages_obj = {};
 	user_fork_repo_languages_obj = {};
 	user_source_repo_languages_obj = {};
 	user_mirror_repo_languages_obj = {};
+	repo_languages_obj = {}; // to store the data for 'all' option 
 
 	$('.repo-list-item').each(function(){
 		if($(this).hasClass('fork')){
@@ -84,76 +82,50 @@ function update_repo_info(){
 
 		if(language_text != ''){
 			x = add_language_count(x, language_text);
-			if(user_repo_languages_obj.hasOwnProperty(language_text)){
-				user_repo_languages_obj[language_text] += 1;
+			if(repo_languages_obj.hasOwnProperty(language_text)){
+				repo_languages_obj[language_text] += 1;
 			} else {
-				user_repo_languages_obj[language_text] = 1;
+				repo_languages_obj[language_text] = 1;
 			}
 		}
 	});
 
+	console.log(user_fork_repo_languages_obj);
+	console.log(user_source_repo_languages_obj);
+	console.log(user_mirror_repo_languages_obj);
+	console.log(repo_languages_obj);
+	
 	user_repo_languages = [];
-	for(var language in user_repo_languages_obj){
-		new_obj = {
-			'lang': 	language,
-			'all': 		parseInt(user_repo_languages_obj[language]) || 0,
-			'forks': 	parseInt(user_fork_repo_languages_obj[language])  || 0,
-			'sources': 	parseInt(user_source_repo_languages_obj[language]) || 0,
-			'mirrors': 	parseInt(user_mirror_repo_languages_obj[language]) || 0,
-		}
-		user_repo_languages.push(new_obj);
+	for(var language in user_fork_repo_languages_obj){
+		// new_obj = {
+		// 	'lang': language,
+		// 	'num': user_repo_languages_obj[language]
+		// }
+		// user_repo_languages.push(new_obj);
+		user_repo_languages.push([language, user_fork_repo_languages_obj[language]])
 	}
-}
-
-function update_repo_stats(mode, language_selected=''){
-	console.log(mode);
-	console.log(user_repo_languages);
 
 	user_repo_languages.sort(function(a, b) {
-		return b[mode] - a[mode];
+		return b[1] - a[1];
 	});
 
 	console.log(user_repo_languages);
 
-	language_select_tag.html('');
-	static_options_html = '<option value="all" disabled selected>Select Language</option><option value="all"><strong>All Languages</strong></option>';
-	language_select_tag.append(static_options_html);
-
 	for(language in user_repo_languages){
-		lang_obj = user_repo_languages[language];
-
-		language_str = lang_obj.lang;
-		language_count = lang_obj[mode];
-		
-		console.log(user_repo_languages[language]);
-		console.log(language_count);
-
-		if(language_selected == language_str.toLowerCase())
-			html_to_injected = "<option selected>" + language_str + " (" + language_count + ")</option>";
-		else
-			html_to_injected = "<option>" + language_str + " (" + language_count + ")</option>";
-
+		language_str = user_repo_languages[language].lang;
+		language_count = user_repo_languages[language].num;
+		html_to_injected = "<option>" + language_str + " (" + language_count + ")</option>";
 		language_select_tag.append(html_to_injected);
 	}
-}
-
-function get_selected_language(){
-	var language = $('body').find('#all_languages').val();
-	
-	if(language == null)
-		return '';
-
-	language_array = language.toLowerCase().split(' ');
-	language_array.pop();
-	language = language_array.join(' ');
-
-	return language;
 }
 
 function classify_repo(github_filter=All){
 	$('.repo-list').addClass('disabled_div');
 	setTimeout(function() {
-		selected_language = get_selected_language();
+		selected_language = $('body').find('#all_languages').val().toLowerCase();
+		selected_language_array = selected_language.split(' ');
+		selected_language_array.pop();
+		selected_language = selected_language_array.join(' ');
 		// console.log(selected_language);
 		if(selected_language == ''){
 			show_languages(selected_language, true, github_filter);
@@ -167,13 +139,12 @@ function classify_repo(github_filter=All){
 }
 
 
-select_language_tag_html = '<select id="all_languages" class="btn"></select>';
+select_language_tag_html = '<select id="all_languages" class="btn"><option value="" disabled selected>Select Language</option><option value="all"><strong>All Languages</strong></option></select>';
 github_navbar_div = $('.filter-bar');
 github_navbar_div.append(select_language_tag_html);
 language_select_tag = github_navbar_div.find('#all_languages');
 
 update_repo_info();
-update_repo_stats('all');
 
 language_select_tag.change(function(){
 	github_filter = $('body').find('.filter-selected').text();
@@ -182,11 +153,7 @@ language_select_tag.change(function(){
 
 $('.js-repo-filter-tab').on('click', function(){
 	github_filter = $(this).text();
-	// language_selected = $('body').find('#all_languages').val().toLowerCase();
 	console.log(github_filter);
-
-	update_repo_stats(github_filter.toLowerCase(), get_selected_language());
-
 	classify_repo(github_filter);
 });
 
